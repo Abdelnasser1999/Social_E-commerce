36b9fab33220d1f24be7ed48d2b902805192c58b
Nasser <nanilion2012@gmail.com>
2021-12-30 12:50:38 +0200

    Initial commit

---
+++ app/src/main/java/com/nasser/ma99/social_e_commerce/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nasser.ma99.social_e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

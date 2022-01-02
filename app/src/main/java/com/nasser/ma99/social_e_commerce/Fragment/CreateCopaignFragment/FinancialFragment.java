55d873bcdd95687ad07db4eb2195a1868d641f46
Dev Mohammed <d.mohammed2022@gmail.com>
2022-01-02 10:09:35 +0200

    new

--- app/src/main/java/com/nasser/ma99/social_e_commerce/Fragment/CreateCopaignFragment/FinancialFragment.java
+++ app/src/main/java/com/nasser/ma99/social_e_commerce/Fragment/CreateCopaignFragment/FinancialFragment.java
@@ -9,28 +9,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nasser.ma99.social_e_commerce.R;
-import com.nasser.ma99.social_e_commerce.databinding.FragmentPrelaodedBinding;
+import com.nasser.ma99.social_e_commerce.databinding.FragmentFinancialBinding;
 
 
-public class PrelaodedFragment extends Fragment {
+public class FinancialFragment extends Fragment {
 
-  FragmentPrelaodedBinding binding;
+    FragmentFinancialBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentPrelaodedBinding.inflate(inflater, container, false);
+
+        binding = FragmentFinancialBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
 
-        binding.btNextPayment.setOnClickListener(view1 -> {
+        binding.btNextBilling.setOnClickListener(view1 -> {
 
             getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentContainerView,new PaymentFragment()).commit();
-
-
+                    .replace(R.id.fragmentContainerView, new BillingFragment()).commit();
         });
 
 
-        return  view;
+        return view;
     }
 }
--- app/src/main/java/com/nasser/ma99/social_e_commerce/Fragment/CreateCopaignFragment/PaymentFragment.java
+++ app/src/main/java/com/nasser/ma99/social_e_commerce/Fragment/CreateCopaignFragment/PaymentFragment.java
@@ -9,28 +9,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.nasser.ma99.social_e_commerce.R;
-import com.nasser.ma99.social_e_commerce.databinding.FragmentPrelaodedBinding;
+import com.nasser.ma99.social_e_commerce.databinding.FragmentPaymentBinding;
 
+public class PaymentFragment extends Fragment {
 
-public class PrelaodedFragment extends Fragment {
-
-  FragmentPrelaodedBinding binding;
+    FragmentPaymentBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentPrelaodedBinding.inflate(inflater, container, false);
+        binding = FragmentPaymentBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
-
-        binding.btNextPayment.setOnClickListener(view1 -> {
+        binding.btNextFinancial.setOnClickListener(view1 -> {
 
             getActivity().getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragmentContainerView,new PaymentFragment()).commit();
-
+                    .replace(R.id.fragmentContainerView, new FinancialFragment()).commit();
 
         });
 
 
-        return  view;
+        return view;
     }
 }

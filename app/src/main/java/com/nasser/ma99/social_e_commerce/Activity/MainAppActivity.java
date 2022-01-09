d67ecafee6932d24f6dfebb094dd9cbda0468dd2
Mohammed Abd <d.mohammed2022@gmail.com>
2022-01-09 18:14:08 +0000

    new

---
+++ app/src/main/java/com/nasser/ma99/social_e_commerce/Activity/MainAppActivity.java
@@ -0,0 +1,38 @@
+package com.nasser.ma99.social_e_commerce.Activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.nasser.ma99.social_e_commerce.R;
+import com.nasser.ma99.social_e_commerce.databinding.ActivityMainAppBinding;
+
+public class MainAppActivity extends AppCompatActivity {
+
+    private ActivityMainAppBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainAppBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_settings, R.id.navigation_chat, R.id.navigation_home,R.id.navigation_influencer,R.id.navigation_compaign)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_app);
+      //  NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+}
